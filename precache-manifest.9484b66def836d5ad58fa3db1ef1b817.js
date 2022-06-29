96602e883e6b0fed6504a2ba5c7525f0e46666f5
DebugSp07 <91491981+DebugSp07@users.noreply.github.com>
2022-06-29 12:51:47 -0700

    Updates

---
+++ precache-manifest.9484b66def836d5ad58fa3db1ef1b817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8144322259c56d9716eb",
+    "url": "/prct1/static/css/main.0c70b30a.chunk.css"
+  },
+  {
+    "revision": "8144322259c56d9716eb",
+    "url": "/prct1/static/js/main.81443222.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/prct1/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "109cecdacbf245e06942",
+    "url": "/prct1/static/js/2.109cecda.chunk.js"
+  },
+  {
+    "revision": "ec3c2a558d025a74d86d",
+    "url": "/prct1/static/js/runtime~main.ec3c2a55.js"
+  },
+  {
+    "revision": "e68f04285015606b3d8b682f6558304c",
+    "url": "/prct1/index.html"
+  }
+];
